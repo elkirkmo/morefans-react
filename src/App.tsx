70d43b9c41bd2ffde92545fb348b753a40bd3204
Chris Kirkham <me@chriskirkham.com>
2020-06-17 23:09:22 -0600

    chore(): fixed looping

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Team from './components/Team';
 import data from './data/nfl';
 import './App.css';
 
 const App = () => {
   const [currentTeam, setCurrentTeam] = useState(0);
-
-  useEffect(() => {
-    if (currentTeam === data.length - 1) setCurrentTeam(0);
-  }, [currentTeam]);
   return (
     <div className="App" id="nfl">
-      <header>
-        <h1>More Fans</h1>
-      </header>
-      {currentTeam > 0 && (
-        <button onClick={() => setCurrentTeam(currentTeam - 1)}>
-          Previous Team
-        </button>
-      )}
       <Team {...data[currentTeam]} />
-
-      <button onClick={() => setCurrentTeam(currentTeam + 1)}>Next Team</button>
+      <button
+        onClick={() =>
+          currentTeam > 0
+            ? setCurrentTeam(currentTeam - 1)
+            : setCurrentTeam(data.length - 1)
+        }
+      >
+        Previous Team
+      </button>
+      <button
+        onClick={() =>
+          currentTeam < data.length - 1
+            ? setCurrentTeam(currentTeam + 1)
+            : setCurrentTeam(0)
+        }
+      >
+        Next Team
+      </button>
+      <footer></footer>
     </div>
   );
 };
